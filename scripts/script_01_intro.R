2dcc8a09cb460cc327ff68150d7a1336915f0739
Elias DeLeon <juggereggnog@gmail.com>
2020-09-12 18:22:45 -0400

    found swing margins

--- scripts/script_01_intro.R
+++ scripts/script_01_intro.R
@@ -2,6 +2,10 @@ library(usmap)
 library(tidyverse)
 
 popvote <- read_csv("data/intro/popvote_1948-2016.csv")
-popvote_state <- read_csv("data/intro/popvote_bystate_1948-2016.csv")
-electvote <- read_csv("data/intro/popvote_bystate_1948-2016.csv")
 
+popvote_state <- read_csv("data/intro/popvote_bystate_1948-2016.csv") %>% 
+  arrange(state, year)
+
+popvote_state_2 <- popvote_state %>% 
+  mutate(prev_D_pv2p = lag(D_pv2p, n = 1),
+         swing = D_pv2p - prev_D_pv2p)
