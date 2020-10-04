aaea3692bf5c8c1b514d9fee736a42dad403fa0a
Elias DeLeon <juggereggnog@gmail.com>
2020-10-03 23:15:45 -0400

    final commit

--- scripts/script_04_incumbency.R
+++ scripts/script_04_incumbency.R
@@ -50,17 +50,7 @@ time_chg <- popvote %>%
 
 
 
-
-
 ########################### PREDICTION WORKFLOW ################################
-
-A <- 47.26
-NETAPP <- 6
-Q2GDP <- 1.2
-TERM1INC <- 0
-
-V <- A + 0.108 * NETAPP + 0.543 * Q2GDP + 4.313 * TERM1INC
-
 
 # Joining poll data to popular vote data (I'm justifying rounding down to 4
 # weeks due to the massive increase in mail-in voting)
@@ -68,7 +58,8 @@ omega <- popvote %>%
   full_join(pollavg %>% 
               filter(weeks_left == 4) %>% 
               group_by(year, party) %>% 
-              summarize(avg_support = mean(avg_support)))
+              summarize(avg_support = mean(avg_support))) %>% 
+  filter(year >= 1972)
 
 
 
@@ -78,7 +69,6 @@ omega_poll_inc <- omega_poll[omega_poll$incumbent_party,]
 omega_poll_chl <- omega_poll[!omega_poll$incumbent_party,]
 mod_poll_inc <- lm(pv ~ avg_support, data = omega_poll_inc)
 mod_poll_chl <- lm(pv ~ avg_support, data = omega_poll_chl)
-
 mod_tfc_inc <- lm(pv2p ~ GDP_growth_qt + net_approve + incumbent, data = time_chg)
 
 
@@ -86,33 +76,74 @@ mod_tfc_inc <- lm(pv2p ~ GDP_growth_qt + net_approve + incumbent, data = time_ch
 ### Step 2: Summarize key statistics and interpret them
 summary(mod_poll_inc)
 summary(mod_poll_chl)
-
 summary(mod_tfc_inc)
 
 
 
 ### Step 3: In-sample testing (Mean Standard Error (MSE))
 mean(abs(mod_poll_inc$residuals))
+
+## Plotting (Incumbent) Poll Predicted vs. True
+ggplot(mapping = aes(x = mod_poll_inc$fitted.values, y = omega_poll_inc$pv, label = omega_poll_inc$year)) + 
+  geom_text() +
+  geom_abline(slope = 1, lty = 2) +
+  geom_vline(xintercept = 50, alpha = 0.2) + 
+  geom_hline(yintercept = 50, alpha = 0.2) +
+  xlab("Predicted Two-Party Vote Share") +
+  ylab("Actual Two-Party Vote Share") +
+  theme_bw()
+
+ggsave("poll_inc_pvt.png", path = "figures/incumbency", height = 4, width = 6)
+
+
+
 mean(abs(mod_poll_chl$residuals))
 
+## Plotting (Challenger) Poll Predicted vs. True
+ggplot(mapping = aes(x = mod_poll_chl$fitted.values, y = omega_poll_chl$pv, label = omega_poll_inc$year)) + 
+  geom_text() +
+  geom_abline(slope = 1, lty = 2) +
+  geom_vline(xintercept = 50, alpha = 0.2) + 
+  geom_hline(yintercept = 50, alpha = 0.2) +
+  xlab("Predicted Two-Party Vote Share") +
+  ylab("Actual Two-Party Vote Share") +
+  theme_bw()
+
+ggsave("poll_chl_pvt.png", path = "figures/incumbency", height = 4, width = 6)
+
+
+
 mean(abs(mod_tfc_inc$residuals))
+
+## Plotting Time for Change Predicted vs. True
+ggplot(mapping = aes(x = mod_tfc_inc$fitted.values, y = time_chg$pv2p, label = time_chg$year)) + 
+  geom_text() +
+  geom_abline(slope = 1, lty = 2) +
+  geom_vline(xintercept = 50, alpha = 0.2) + 
+  geom_hline(yintercept = 50, alpha = 0.2) +
+  xlab("Predicted Two-Party Vote Share") +
+  ylab("Actual Two-Party Vote Share") +
+  theme_bw()
+
+ggsave("tfc_pvt.png", path = "figures/incumbency", height = 4, width = 6)
 
 
 
 ### Step 4: Out-of-sample testing
-all_years <- seq(from = 1980, to = 2016, by = 4)
+all_years <- seq(from = 1992, to = 2016, by = 4)
 outsamp_dflist <- lapply(all_years, function(year){
   
   true_inc <- unique(omega$pv[omega$year == year & omega$incumbent_party])
   true_chl <- unique(omega$pv[omega$year == year & !omega$incumbent_party])
   true_tfc_inc <- unique(time_chg$pv2p[time_chg$year == year])
   
-  ##poll model out-of-sample prediction
+  ## Poll model out-of-sample prediction
   mod_poll_inc_ <- lm(pv ~ avg_support, data = omega_poll_inc[omega_poll_inc$year != year,])
   mod_poll_chl_ <- lm(pv ~ avg_support, data = omega_poll_chl[omega_poll_chl$year != year,])
   pred_poll_inc <- predict(mod_poll_inc_, omega_poll_inc[omega_poll_inc$year == year,])
   pred_poll_chl <- predict(mod_poll_chl_, omega_poll_chl[omega_poll_chl$year == year,])
   
+  ## "Time for Change" model out-of-sample prediction
   mod_tfc_inc_ <- lm(pv2p ~ GDP_growth_qt + net_approve + incumbent, data = time_chg[time_chg$year != year,])
   pred_tfc_inc <- predict(mod_tfc_inc_, time_chg[time_chg$year == year,])
   
@@ -125,11 +156,10 @@ outsamp_dflist <- lapply(all_years, function(year){
 })
 
 
-
 outsamp_df <- do.call(rbind, outsamp_dflist)
-colMeans(abs(outsamp_df[2:3]), na.rm = T)
-colMeans(outsamp_df[4:5], na.rm = T) ### classification accuracy
-outsamp_df[,c("year","poll_winner_correct")]
+colMeans(abs(outsamp_df[2:3]), na.rm = TRUE)
+colMeans(outsamp_df[4:5], na.rm = TRUE) ### classification accuracy
+outsamp_df[,c("year", "poll_winner_correct", "tfc_winner_correct")]
 
 
 
@@ -149,4 +179,3 @@ tfc_2020_inc <- data.frame(GDP_growth_qt = -0.0949, net_approve = -19, incumbent
 
 (pred_tfc_2020_inc <- predict(mod_tfc_inc, tfc_2020_inc, 
                               interval = "prediction", level = 0.95))
-
