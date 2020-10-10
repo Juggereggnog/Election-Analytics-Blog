44aff6af10820081b0a36db3906218e01838554a
Elias DeLeon <juggereggnog@gmail.com>
2020-10-10 19:53:37 -0400

    plotted geofacet of state prediction distributions

--- scripts/script_05_air_war.R
+++ scripts/script_05_air_war.R
@@ -42,32 +42,17 @@ vep %>%
 #TODO
 
 
-VEP_PA_2020 <- as.integer(vep$VEP[vep$state == "Pennsylvania" & vep$year == 2016])
 
-PA_R <- poll_pvstate_vep %>% filter(state=="Pennsylvania", party=="republican")
-PA_D <- poll_pvstate_vep %>% filter(state=="Pennsylvania", party=="democrat")
 
-## Fit D and R models
-PA_R_glm <- glm(cbind(R, VEP-R) ~ avg_poll, PA_R, family = binomial)
-PA_D_glm <- glm(cbind(D, VEP-D) ~ avg_poll, PA_D, family = binomial)
 
-## Get predicted draw probabilities for D and R
-prob_Rvote_PA_2020 <- predict(PA_R_glm, newdata = data.frame(avg_poll=44.5), type="response")[[1]]
-prob_Dvote_PA_2020 <- predict(PA_D_glm, newdata = data.frame(avg_poll=50), type="response")[[1]]
 
-## Get predicted distribution of draws from the population
-sim_Rvotes_PA_2020 <- rbinom(n = 10000, size = VEP_PA_2020, prob = prob_Rvote_PA_2020)
-sim_Dvotes_PA_2020 <- rbinom(n = 10000, size = VEP_PA_2020, prob = prob_Dvote_PA_2020)
 
-## Simulating a distribution of election results: Biden PA PV
-hist(sim_Dvotes_PA_2020, xlab="predicted turnout draws for Biden\nfrom 10,000 binomial process simulations", breaks=100)
 
-## Simulating a distribution of election results: Trump PA PV
-hist(sim_Rvotes_PA_2020, xlab="predicted turnout draws for Trump\nfrom 10,000 binomial process simulations", breaks=100)
 
-## Simulating a distribution of election results: Biden win margin
-sim_elxns_PA_2020 <- ((sim_Dvotes_PA_2020-sim_Rvotes_PA_2020)/(sim_Dvotes_PA_2020+sim_Rvotes_PA_2020))*100
-hist(sim_elxns_PA_2020, xlab="predicted draws of Biden win margin (% pts)\nfrom 10,000 binomial process simulations", xlim=c(2, 7.5))
+
+
+
+
 
 
 
@@ -110,8 +95,8 @@ pollstate_2020$avg_poll <- c(40.1, 55.0, 45.2, 49.7, 48.4, 45.1, 45.1, 48.2,
                              55.6, 32.4, 51.1, 41.0, 59.1, 34.8, 33.5, 62.9,
                              50.5, 43.8, NA, NA)
 
-
 s <- unique(poll_pvstate$state)
+
 meow <- lapply(s, function(s){
 
 
@@ -140,54 +125,31 @@ prob_Dvote_s_2020 <- predict(s_D_glm, newdata = data.frame(avg_poll = poll_s_D_2
 sim_Rvotes_s_2020 <- rbinom(n = 10000, size = VEP_s_2020, prob = prob_Rvote_s_2020)
 sim_Dvotes_s_2020 <- rbinom(n = 10000, size = VEP_s_2020, prob = prob_Dvote_s_2020)
 
+## Simulating a distribution of election results: Biden win margin
+sim_elxns_s_2020 <- ((sim_Dvotes_s_2020 - sim_Rvotes_s_2020) / (sim_Dvotes_s_2020 + sim_Rvotes_s_2020)) * 100
+
 
 cbind.data.frame(election_id = 1:10000,
                  state = s,
                  prob_Rvote_s_2020,
                  prob_Dvote_s_2020,
                  sim_Rvotes_s_2020,
-                 sim_Dvotes_s_2020)
+                 sim_Dvotes_s_2020,
+                 sim_elxns_s_2020)
 })
 
 dooby <- do.call(rbind, meow)
 
 
+dooby %>% 
+  ggplot(aes(x = sim_elxns_s_2020)) +
+  facet_geo(~ state) +
+  geom_histogram(bins = 100) +
+  coord_flip()
+
 
 dooby %>% 
-  filter(state == "Alabama") %>% 
-  ggplot(aes(x = sim_Dvotes_s_2020)) + 
-  geom_histogram(bins = 100) +
-  facet_wrap(~ state)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  filter(state == "Virginia") %>% 
+  ggplot(aes(x = sim_elxns_s_2020)) + 
+  geom_histogram(bins = 100)
 
