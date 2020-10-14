deb523c2d3644fb5be3014dc8cea9fa2646044f8
Elias DeLeon <juggereggnog@gmail.com>
2020-10-13 20:30:00 -0400

    initial commit

---
+++ posts/post_06_ground_game.md
@@ -0,0 +1,25 @@
+# Ground Game
+
+# October 17, 2020
+
+
+
+## The Ground Game: A Move to Mobilize
+
+
+The
+
+
+### Presidential Two-Party Popular Vote Shares in U.S. Elections 1948-2016
+
+![Win Margins](../figures/intro/popvote_win_margin.png)
+
+
+The
+
+
+
+## Prediction
+
+
+The
---
+++ scripts/script_06_ground_game.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+
+
+pvcounty <- read_csv("data/ground_game/popvote_bycounty_2012-2016_WI.csv")
+
+fo_county <- read_csv("data/ground_game/fieldoffice_2012_bycounty.csv")
+
+fo_address <- read_csv("data/ground_game/fieldoffice_2012-2016_byaddress.csv")
+
+fo_dems <- read_csv("data/ground_game/fieldoffice_2004-2012_dems.csv")
+
+dem <- read_csv("data/ground_game/demographic_1990-2018.csv")
+
+
+
+
+
+
+######################## DESCRIPTIVE ANALYSIS ##################################
+
+# Meow
+
+
+
+
+
+
+
+
+
+
+######################## PREDICTIVE ANALYSIS ###################################
+
+# Meow
+
+
+
+
+
+
+
+
+
+
+################################## TESTING #####################################
+
+# Meow
