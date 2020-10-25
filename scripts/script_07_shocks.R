60e6f4d24c1dbc844b528caf64eac8423b04e1b2
Elias DeLeon <juggereggnog@gmail.com>
2020-10-24 23:33:04 -0400

    final commit

--- scripts/script_07_shocks.R
+++ scripts/script_07_shocks.R
@@ -16,21 +16,290 @@ covid_nat <- read_csv("data/shocks/national-history.csv")
 
 covid_state <- read_csv("data/shocks/all-states-history.csv")
 
+pollavg <- read_csv("data/shocks/president_polls.csv") %>% 
+  filter(is.na(state),
+         candidate_name == "Donald Trump" | candidate_name == "Joseph R. Biden Jr.") %>% 
+  mutate(created_at = substr(created_at, 1, nchar(created_at) - 6),
+         created_at = mdy(created_at)) %>% 
+  select(question_id, created_at, candidate_name, candidate_party, pct)
+
+# Finding question ids with answers for both Biden and Trump
+goodpolls <- pollavg %>% 
+  count(question_id) %>% 
+  filter(n == 2) %>% 
+  select(question_id)
+
+pollavg <- pollavg %>% 
+  right_join(goodpolls, by = "question_id") %>% 
+  group_by(created_at, candidate_name) %>% 
+  summarize(avg_poll = mean(pct)) %>%
+  filter(candidate_name == "Donald Trump") %>% 
+  select(-candidate_name)
+
+
+# Necessary data from last week to reproduce binomial simulations
+turnout <- read_csv("data/ground_game/turnout_1980-2016.csv") %>% 
+  mutate(turnout_pct = substr(turnout_pct, 1, nchar(turnout_pct) - 1),
+         turnout_pct = as.double(turnout_pct),
+         turnout_pct = ifelse(year == 2016, round(turnout_pct * 100, 1), turnout_pct)) %>% 
+  filter(!is.na(turnout_pct),
+         state != "District of Columbia",
+         state != "United States")
+
+pollstate <- read_csv("data/ground_game/pollavg_bystate_1968-2016.csv")
+
+polls_2020 <- read_csv("data/ground_game/polls_2020_new.csv") %>% 
+  mutate(created_at = substr(created_at, 1, nchar(created_at) - 6),
+         created_at = mdy(created_at)) %>% 
+  arrange(state, created_at) %>% 
+  select(state, created_at, candidate_name, candidate_party, pct)
+
+pvstate <- read_csv("data/ground_game/popvote_bystate_1948-2016.csv")
+
+vep <- read_csv("data/ground_game/vep_1980-2016.csv")
+
+poll_pvstate <- pvstate %>% 
+  inner_join(pollstate %>% 
+               filter(weeks_left == 5) %>% # Code breaks when weeks_left == 3 for reasons unknown (more obs.)
+               group_by(state, year, candidate_name) %>%
+               top_n(1, poll_date)) %>% 
+  mutate(D_pv = (D / total) * 100,
+         R_pv = (R / total) * 100)
+
+poll_pvstate_vep <- poll_pvstate %>% 
+  inner_join(vep)
+
 
 ######################## DESCRIPTIVE ANALYSIS ##################################
 
-# Meow
+
+# Joining data into one splendorous data frame
 covid_app <- approval %>% 
   left_join(covid_nat, by = c("created_at" = "date")) %>% 
-  select(-posNeg, )
+  left_join(pollavg,
+            by = "created_at") %>% 
+  select(created_at, yes, no, avg_poll, everything(), -posNeg, -states) %>% 
+  filter(!is.na(death))
+
+# Balls-and-string graph of approval by date and death toll
+ggplot(covid_app, aes(x = created_at, y = yes, color = positiveIncrease)) +
+  geom_line() +
+  geom_point(aes(size = deathIncrease)) +
+  geom_smooth(method = "lm") +
+  labs(x = "Date",
+       y = "Job Approval",
+       size = "Death Toll",
+       color = "New Cases") +
+  theme_bw()
+
+ggsave("balls_n_string.png", path = "figures/shocks", height = 4, width = 8)
+
+cor(covid_app$positive, covid_app$yes)
+cor(covid_app$death, covid_app$yes)
+
+meow <- lm(yes ~ death + positive, data = covid_app)
+summary(meow)
 
 
-ggplot(covid_app, aes(x = created_at, y = yes)) +
-  geom_line()
+######################## PREDICTIVE ANALYSIS ###################################
 
 
+# Making (relevant) polls_2020 dataframe (polls 5 weeks out)
+pollstate_2020 <- data.frame(ID = 1:100)
+pollstate_2020$state <- state.name
+pollstate_2020 <- pollstate_2020 %>% 
+  arrange(state) %>% 
+  select(-ID)
+pollstate_2020$party <- c("democrat", "republican")
+## Manually coded in FiveThirtyEight state poll avgs alphabetically (two per state)
+pollstate_2020$avg_poll <- c(40.1, 55.0, 45.2, 49.7, 48.4, 45.1, 45.1, 48.2,
+                             62.1, 30.8, 51.1, 41.1, 55.1, 34.3, 57.5, 38.0,
+                             47.9, 46.1, 46.8, 47.0, 56.7, 31.2, 34.6, 59.5,
+                             52.9, 39.8, 38.9, 53.4, 45.8, 46.6, 41.6, 50.5,
+                             38.2, 57.0, 40.4, 51.1, 54.3, 39.0, 61.0, 32.7,
+                             63.9, 29.7, 49.9, 43.0, 50.7, 42.0, 40.7, 52.5,
+                             44.0, 50.7, 42.9, 51.0, NA, NA, 48.8, 42.4,
+                             51.4, 42.8, 55.6, 36.4, 54.1, 41.4, 60.3, 33.4,
+                             47.5, 46.5, 38.0, 55.2, 48.0, 47.0, 34.5, 58.0,
+                             51.0, 39.1, 49.9, 44.5, NA, NA, 43.6, 51.1,
+                             NA, NA, 40.8, 53.2, 45.9, 47.9, 37.0, 49.5,
+                             55.6, 32.4, 51.1, 41.0, 59.1, 34.8, 33.5, 62.9,
+                             50.5, 43.8, NA, NA)
 
 
+s <- unique(poll_pvstate$state)
+
+pollR_sd <- sd(pollstate_2020 %>% 
+                 filter(party == "republican", !is.na(avg_poll)) %>% 
+                 pull(avg_poll)) / 100
+
+pollD_sd <- sd(pollstate_2020 %>% 
+                 filter(party == "democrat", !is.na(avg_poll)) %>% 
+                 pull(avg_poll)) / 100
 
 
-ggsave(".png", path = "figures/shocks", height = 4, width = 8)
+meow <- lapply(s, function(s){
+  
+  VEP_s_2020 <- as.integer(vep$VEP[vep$state == s & vep$year == 2016])
+  
+  poll_s_R_2020 <- pollstate_2020 %>% 
+    filter(state == s, party == "republican") %>% 
+    pull(avg_poll)
+  
+  poll_s_D_2020 <- pollstate_2020 %>% 
+    filter(state == s, party == "democrat") %>% 
+    pull(avg_poll)
+  
+  s_R <- poll_pvstate_vep %>% filter(state == s, party == "republican")
+  s_D <- poll_pvstate_vep %>% filter(state == s, party == "democrat")
+  
+  ## Fit D and R models
+  s_R_glm <- glm(cbind(R, VEP-R) ~ avg_poll, s_R, family = binomial)
+  s_D_glm <- glm(cbind(D, VEP-D) ~ avg_poll, s_D, family = binomial)
+  
+  ## Get predicted draw probabilities for D and R
+  prob_Rvote_s_2020 <- predict(s_R_glm, newdata = data.frame(avg_poll = poll_s_R_2020), type = "response")[[1]]
+  prob_Dvote_s_2020 <- predict(s_D_glm, newdata = data.frame(avg_poll = poll_s_D_2020), type = "response")[[1]]
+  
+  ## Get predicted distribution of draws from the population
+  avg_turnout_s <- turnout %>% 
+    filter(state == s, !is.na(turnout_pct)) %>% 
+    summarize(avg_turnout = mean(turnout_pct) / 100) %>% 
+    pull(avg_turnout)
+  
+  sd_turnout_s <- turnout %>% 
+    filter(state == s, !is.na(turnout_pct)) %>% 
+    summarize(sd_turnout = sd(turnout_pct) / 100) %>% 
+    pull(sd_turnout)
+  
+  ## Creating turnout distribution (LVP = Likely Voter Population)
+  LVP_s_2020 <- rnorm(10000, mean = VEP_s_2020 * avg_turnout_s, sd = VEP_s_2020 * sd_turnout_s)
+  
+  normalR <- rnorm(10000, mean = prob_Rvote_s_2020, sd = pollR_sd)
+  normalD <- rnorm(10000, mean = prob_Dvote_s_2020, sd = pollD_sd)
+  
+  ## Coefficients represent net effect of COVID-19 & proliferated vote-by-mail
+  ## Republicans less wary of COVID-19 (-10%), less prone to voting by mail (+5%)
+  ## Democrats more wary of COVID-19 (-30%), more prone to voting by mail (+19.6%)
+  sim_Rvotes_s_2020 <- rbinom(n = 10000, size = round(0.95 * LVP_s_2020), prob = normalR)
+  sim_Dvotes_s_2020 <- rbinom(n = 10000, size = round(0.896 * LVP_s_2020), prob = normalD)
+  
+  ## Simulating a distribution of election results: Biden win margin
+  sim_elxns_s_2020 <- ((sim_Dvotes_s_2020 - sim_Rvotes_s_2020) / (sim_Dvotes_s_2020 + sim_Rvotes_s_2020)) * 100
+  
+  
+  cbind.data.frame(election_id = 1:10000,
+                   state = s,
+                   prob_Rvote_s_2020,
+                   prob_Dvote_s_2020,
+                   VEP_s_2020,
+                   LVP_s_2020,
+                   normalR,
+                   normalD,
+                   sim_Rvotes_s_2020,
+                   sim_Dvotes_s_2020,
+                   sim_elxns_s_2020)
+})
+
+dooby <- do.call(rbind, meow)
+
+
+dooby <- dooby %>% 
+  filter(!is.na(sim_elxns_s_2020)) %>% 
+  mutate(state_win = case_when(sim_elxns_s_2020 > 0 ~ "win",
+                               sim_elxns_s_2020 < 0 ~ "lose",
+                               TRUE ~ "tie"),
+         state_abb = state.abb[match(state, state.name)],
+         ev = case_when(state_abb == "AL" ~ 9,
+                        state_abb == "AK" ~ 3,
+                        state_abb == "AZ" ~ 11,
+                        state_abb == "AR" ~ 6,
+                        state_abb == "CA" ~ 55,
+                        state_abb == "CO" ~ 9,
+                        state_abb == "CT" ~ 7,
+                        state_abb == "DE" ~ 3,
+                        state_abb == "FL" ~ 29,
+                        state_abb == "GA" ~ 16,
+                        state_abb == "HI" ~ 4,
+                        state_abb == "ID" ~ 4,
+                        state_abb == "IL" ~ 20,
+                        state_abb == "IN" ~ 11,
+                        state_abb == "IA" ~ 6,
+                        state_abb == "KS" ~ 6,
+                        state_abb == "KY" ~ 6,
+                        state_abb == "LA" ~ 8,
+                        state_abb == "ME" ~ 4,
+                        state_abb == "MD" ~ 10,
+                        state_abb == "MA" ~ 11,
+                        state_abb == "MI" ~ 16,
+                        state_abb == "MN" ~ 16,
+                        state_abb == "MS" ~ 6,
+                        state_abb == "MO" ~ 10,
+                        state_abb == "MT" ~ 3,
+                        state_abb == "NE" ~ 5,
+                        state_abb == "NV" ~ 6,
+                        state_abb == "NH" ~ 4,
+                        state_abb == "NJ" ~ 14,
+                        state_abb == "NM" ~ 5,
+                        state_abb == "NY" ~ 29,
+                        state_abb == "NC" ~ 15,
+                        state_abb == "ND" ~ 3,
+                        state_abb == "OH" ~ 18,
+                        state_abb == "OK" ~ 7,
+                        state_abb == "OR" ~ 7,
+                        state_abb == "PA" ~ 20,
+                        state_abb == "SC" ~ 9,
+                        state_abb == "TN" ~ 11,
+                        state_abb == "TX" ~ 38,
+                        state_abb == "UT" ~ 6,
+                        state_abb == "VA" ~ 13,
+                        state_abb == "WA" ~ 12,
+                        state_abb == "WV" ~ 5,
+                        state_abb == "WI" ~ 10,
+                        TRUE ~ 999),
+         ev_won = ifelse(state_win == "win", ev, 0),
+         ev_lost = 522 - ev_won)
+
+
+dooby %>% 
+  ggplot(aes(x = sim_elxns_s_2020, fill = state_win)) +
+  facet_geo(~ state_abb, scales = "free") +
+  geom_histogram(bins = 100) +
+  labs(x = "Democratic Win Margin",
+       y = "Number of Simulations",
+       fill = "Dem Results") +
+  theme_bw()
+
+ggsave("better_binomial.png", path = "figures/shocks", height = 6, width = 10)
+
+
+################################## TESTING #####################################
+
+
+# Calculating Electoral College votes for each simulated election
+ev_dist <- data.frame()
+for (i in 1:10000) {
+  
+  
+  elec_res <- dooby %>% 
+    filter(election_id == i) %>% 
+    summarize(elec_res = sum(ev_won)) %>% 
+    pull(elec_res)
+  
+  
+  ev_dist <- rbind(ev_dist,
+                   cbind(election_id = i,
+                         election_result = elec_res))
+}
+
+# Plotting distribution of results
+ev_dist %>% 
+  ggplot(aes(x = election_result, fill = (election_result >= 262))) +
+  geom_histogram(bins = 60) +
+  geom_vline(xintercept = 262) +
+  theme_bw() +
+  labs(x = "# Electoral College Votes",
+       y = "# of Elections",
+       fill = "Biden Wins")
+
+ggsave("election_results.png", path = "figures/shocks", height = 4, width = 8)
